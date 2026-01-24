d2f03f6706f503259cb8c9a126b5c8fbb76f3e49
Bhavishya Mohan Aggarwal <handballaggarwal@gmail.com>
2026-01-24 12:24:40 +0530

    style: move App.css to styles directory and update imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './styles/App.css'
 import Hero from './components/Hero'
 import SearchBar from './components/SearchBar'
 import PriceFilter from './components/PriceFilter'
